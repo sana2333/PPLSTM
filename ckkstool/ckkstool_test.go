dd3227aaf24f88068084313bd342fb5071fc585e
sana2333 <87567251+sana2333@users.noreply.github.com>
2026-06-11 10:53:52 +0800

    commit

--- ckkstool/ckkstool_test.go
+++ ckkstool/ckkstool_test.go
@@ -10,9 +10,74 @@ import (
 	"github.com/tuneinsight/lattigo/v6/schemes/ckks"
 )
 
-// go test -v -run ^TestPCMMDiagonal$ -timeout=20m lstm/ckkstool
-func TestPCMMDiagonal(t *testing.T) {
+// go test -v -run ^TestMatrixMultiply$ -timeout=20m lstm/ckkstool
+func TestMatrixMultiply(t *testing.T) {
+	ckksTool, err := NewCKKSToolForConfig(512, 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("初始化成功")
+	}
+	// ckksTool.GenerateBatchGaloisKeys(512, 64)
 
+	m, n, p := 512, 64, 64
+	x := make([][]float64, m)
+	w := make([][]float64, n)
+	for i := 0; i < m; i++ {
+		x[i] = make([]float64, p)
+		for j := 0; j < p; j++ {
+			x[i][j] = rand.Float64()
+		}
+	}
+	for i := 0; i < n; i++ {
+		w[i] = make([]float64, p)
+		for j := 0; j < p; j++ {
+			w[i][j] = rand.Float64()
+		}
+	}
+
+	fmt.Println("数据生成成功")
+
+	xcol := utils.ColCoding(x)
+	ptx := ckks.NewPlaintext(ckksTool.Params, ckksTool.Params.MaxLevel())
+	ckksTool.End.Encode(xcol, ptx)
+	ctx, _ := ckksTool.Enc.EncryptNew(ptx)
+
+	var all time.Duration
+
+	for i := 0; i < 8; i++ {
+		start := time.Now()
+		res := ckksTool.MatrixMultiplyPCMMDiagonalBSGS(m, ctx, w, 8)
+		// res := ckksTool.MatrixMultiplyWithWorkers(m, ctx, w, 16)
+		elapsed := time.Since(start)
+		fmt.Println("矩阵乘法时间:", elapsed)
+		all += elapsed
+		// ckksTool.LogCiphertextInfo(res, "res", 0)
+		ckksTool.DecToFloat64(res)
+		// res = ckksTool.MatrixMultiplyWithWorkers(m, ctx, w, 16)
+		// ckksTool.DecToFloat64(res)
+	}
+	fmt.Println("all time:", all)
+	// start := time.Now()
+	// res := ckksTool.MatrixMultiplyCPMMDiagonalBSGS(m, ctx, w, 1)
+	// // res := ckksTool.MatrixMultiplyWithWorkers(m, ctx, w, 16)
+	// elapsed := time.Since(start)
+	// fmt.Println("矩阵乘法时间:", elapsed)
+	// ckksTool.LogCiphertextInfo(res, "res", 0)
+	// ckksTool.DecToFloat64(res)
+
+	y := make([]float64, 5)
+	for i := 0; i < 5; i++ {
+		y[i] = 0
+		for j := 0; j < n; j++ {
+			y[i] += x[i][j] * w[j][0]
+		}
+	}
+	fmt.Println("pt:", y)
+}
+
+// go test -v -run ^TestMatrixMultiply1$ -timeout=20m lstm/ckkstool
+func TestMatrixMultiply1(t *testing.T) {
 	m, n, p := 512, 64, 64
 	x := make([][]float64, m)
 	w := make([][]float64, n)
@@ -32,7 +97,7 @@ func TestPCMMDiagonal(t *testing.T) {
 	wt := utils.Transpose(w)
 	fmt.Println("数据生成成功")
 
-	ckksTool, err := NewCKKSTool()
+	ckksTool, err := NewCKKSToolForConfig(512, 64)
 	if err != nil {
 		fmt.Println(err)
 	} else {
@@ -49,7 +114,7 @@ func TestPCMMDiagonal(t *testing.T) {
 	ctx1, _ := ckksTool.Enc.EncryptNew(ptx1)
 
 	start := time.Now()
-	res := ckksTool.MatrixMultiplyPCMMDiagonal(m, ctx1, utils.PadMatrix(wt, n, n), 2)
+	res := ckksTool.MatrixMultiplyPCMMDiagonalBSGS_old(m, ctx1, utils.PadMatrix(wt, n, n), 2)
 	elapsed := time.Since(start)
 	fmt.Println("矩阵乘法1时间:", elapsed)
 
@@ -61,6 +126,15 @@ func TestPCMMDiagonal(t *testing.T) {
 	res = ckksTool.MatrixMultiplyPCMMDiagonalBSGS(m, ctx1, utils.PadMatrix(wt, n, n), 2)
 	elapsed = time.Since(start)
 	fmt.Println("矩阵乘法2时间:", elapsed)
+
+	ckksTool.DecToFloat64(res)
+	ckksTool.Eval.RescaleTo(res, ctx.Scale, res)
+	fmt.Println(res.Level(), " ", res.Scale)
+
+	start = time.Now()
+	res = ckksTool.MatrixMultiplyWithWorkers(m, ctx1, utils.PadMatrix(wt, n, n), 2)
+	elapsed = time.Since(start)
+	fmt.Println("矩阵乘法3时间:", elapsed)
 
 	ckksTool.DecToFloat64(res)
 	ckksTool.Eval.RescaleTo(res, ctx.Scale, res)
@@ -79,137 +153,9 @@ func TestPCMMDiagonal(t *testing.T) {
 	fmt.Println(y)
 }
 
-// go test -v -run ^TestMatrixMultiply1$ -timeout=20m lstm/ckkstool
-func TestMatrixMultiply(t *testing.T) {
-	m := 256
-
-	params := utils.GetParams("dbpedia", 1)
-	fmt.Println("load params finish")
-	x, _ := utils.LoadTxtFloatVector("D:\\Code\\Golang\\lstm\\result\\dbpedia_rms_h_30.txt")
-	w1 := params.FCWeight
-	fmt.Println(len(w1))
-	w, b := params.FCWeight, utils.Repeat(m, params.FCBias)
-	fmt.Println("load x finish")
-	fmt.Println(params.FCBias)
-	fmt.Println(len(w), len(w[0]), len(b))
-
-	ckksTool, err := NewCKKSTool()
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("初始化成功")
-	}
-
-	ptx := ckks.NewPlaintext(ckksTool.Params, ckksTool.Params.MaxLevel())
-	ckksTool.End.Encode(x, ptx)
-	ctx, _ := ckksTool.Enc.EncryptNew(ptx)
-	ckksTool.DecToFloat64(ctx)
-
-	start := time.Now()
-	res := ckksTool.MatrixMultiplyWithWorkers(m, ctx, w, 4)
-	elapsed := time.Since(start)
-	fmt.Println("矩阵乘法时间:", elapsed)
-	ckksTool.DecToFloat64(res)
-	ckksTool.Eval.Add(res, b, res)
-	ckksTool.DecToFloat64(res)
-
-	logRes := make([]float64, ckksTool.Params.MaxSlots())
-	ckksTool.End.Decode(ckksTool.Dec.DecryptNew(res), logRes)
-	utils.SaveDataToFile(logRes, fmt.Sprintf("%s_rms_%d.txt", "dbpedia", 30))
-}
-
-func TestMatrixMultiply1(t *testing.T) {
-	ckksTool, err := NewCKKSTool()
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("初始化成功")
-	}
-
-	m, n, p := 256, 4, 128
-	// x := make([][]float64, m)
-	w := make([][]float64, n)
-	b := make([]float64, n)
-	// for i := 0; i < m; i++ {
-	// 	x[i] = make([]float64, p)
-	// 	for j := 0; j < p; j++ {
-	// 		x[i][j] = rand.Float64()
-	// 	}
-	// }
-	for i := 0; i < n; i++ {
-		b[i] = rand.Float64()
-		w[i] = make([]float64, p)
-		for j := 0; j < p; j++ {
-			w[i][j] = rand.Float64()
-		}
-	}
-
-	x, _ := utils.LoadTxtFloatVector("D:\\Code\\Golang\\lstm\\result\\agnews_rms_h_30.txt")
-
-	fmt.Println("数据生成成功")
-
-	// xcol := utils.ColCoding(x)
-	ptx := ckks.NewPlaintext(ckksTool.Params, ckksTool.Params.MaxLevel())
-	// ckksTool.End.Encode(xcol, ptx)
-	ckksTool.End.Encode(x, ptx)
-	ctx, _ := ckksTool.Enc.EncryptNew(ptx)
-	fmt.Println(ctx.Level(), " ", ctx.Scale)
-
-	start := time.Now()
-	res := ckksTool.MatrixMultiplyWithWorkers(m, ctx, w, 4)
-	elapsed := time.Since(start)
-	fmt.Println("矩阵乘法时间:", elapsed)
-	ckksTool.Eval.Add(res, utils.Repeat(m, b), res)
-
-	ckksTool.DecToFloat64(res)
-
-	ckksTool.Eval.RescaleTo(res, ctx.Scale, res)
-	fmt.Println(res.Level(), " ", res.Scale)
-
-	y := make([]float64, 10)
-	for i := 0; i < 10; i++ {
-		for j := 0; j < p; j++ {
-			y[i] += x[i+j*m] * w[0][j]
-		}
-		y[i] += b[0]
-	}
-	fmt.Println(y)
-}
-
-// go test -v -run ^TestOptimizedFit$ -timeout=20m lstm/ckkstool
-func TestOptimizedFit(t *testing.T) {
-	rsqrt := []float64{2.50005249, -3.20120857, 2.01693233, -0.4280573}
-	rsqrtRange := [2]float64{0.01, 2.4}
-
-	ckksTool, err := NewCKKSTool()
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("初始化成功")
-	}
-
-	m, p := 512, 50
-	x := make([][]float64, m)
-	for i := 0; i < m; i++ {
-		x[i] = make([]float64, p)
-		for j := 0; j < p; j++ {
-			x[i][j] = rand.Float64()
-		}
-	}
-	fmt.Println("数据生成成功")
-
-	xcol := utils.ColCoding(x)
-	ptx := ckks.NewPlaintext(ckksTool.Params, ckksTool.Params.MaxLevel())
-	ckksTool.End.Encode(xcol, ptx)
-	ctx, _ := ckksTool.Enc.EncryptNew(ptx)
-
-	ctx = ckksTool.OptimizedFit(ctx, rsqrt, rsqrtRange)
-	ckksTool.DecToFloat64(ctx)
-}
-
 // go test -v -run ^TestBootstrapping$ -timeout=20m lstm/ckkstool
 func TestBootstrapping(t *testing.T) {
-	ckksTool, err := NewCKKSTool()
+	ckksTool, err := NewCKKSToolForConfig(512, 64)
 	if err != nil {
 		fmt.Println(err)
 	} else {
